0ed717a74db353da73935cf679e89bf8d6bb708c
plocobio <91017583+plocobio@users.noreply.github.com>
2024-05-07 17:25:43 +0200

    Debugueando recombinación

--- RRAM_Simulation.py
+++ RRAM_Simulation.py
@@ -1,7 +1,8 @@
 import glob
-from re import T
+import pandas as pd
 import matplotlib.pyplot as plt
 
+from re import T
 from RRAM import *
 from PIL import Image
 from tqdm import tqdm
@@ -9,15 +10,19 @@ from io import BytesIO
 from icecream import ic
 
 
+# Creo el excel donde voy a sacar todos los datos
+df = pd.DataFrame(columns=['Tiempo simulacion', 'Voltaje', 'Campo Eléctrico', 'Corriente', 'Temperatura',
+                  'Probabilidad de Generación', 'Probabilidad de Recombinación', 'Percolacion'])
+
 # comienzo la simulación montecaarlo
 
 espesor_dispositivo = 10        # nm
-Atom_size = 0.25                 # nm
+Atom_size = 0.5                 # nm
 
 eje_x = round(espesor_dispositivo / Atom_size)
 eje_y = round(espesor_dispositivo / Atom_size)
 
-num_trampas = 15
+num_trampas = 10
 
 # FIXME: Hay una zona donde nunca se ponen trampas
 actual_state = Generation.initial_state(eje_x, eje_y, num_trampas)
@@ -27,12 +32,12 @@ actual_state = Generation.initial_state(eje_x, eje_y, num_trampas)
 RepresentateState(actual_state, "Configuracion_inicial.png")
 
 total_simulation_time = 1
-num_pasos = 1000000
+num_pasos = 1000
 paso_temporal = total_simulation_time / num_pasos
 
 voltaje_final = 3
 
-paso_guardar = 100
+paso_guardar = 10
 
 configuraciones_matriz = np.zeros((int((num_pasos / paso_guardar)), eje_x, eje_y))
 
@@ -43,14 +48,13 @@ voltaje = 0
 simulation_time = 0
 Corriente = 0
 
-ic.disable()
+# ic.disable()
 
 for k in tqdm(range(0, num_pasos)):
     # Guardo el estado anterior
     last_state = actual_state
 
     simulation_time = paso_temporal*k
-    ic(simulation_time)
 
     # Calculo la corriente
     voltaje += voltaje_final * paso_temporal
@@ -66,10 +70,9 @@ for k in tqdm(range(0, num_pasos)):
         # TODO: REVISAR QUE LA CORRIENTE TIENE LAS UNIDADES CORRECTAS PORQUE NO CUADRAN VALORES.
         Corriente = 10000*CurentSolver.poole_frenkel(Temperatura, Campo_Electrico)
 
-    ic(Corriente)
     # Obtengo los valores del campo eléctrico y la temperatura
-    Campo_Electrico = ic(SimpleElectricField(voltaje, espesor_dispositivo*1e-9))
-    Temperatura = ic(Temperature_Joule(voltaje, Corriente, T_0=350))
+    Campo_Electrico = SimpleElectricField(voltaje, espesor_dispositivo*1e-9)
+    Temperatura = Temperature_Joule(voltaje, Corriente, T_0=350)
 
     # Calculo la probabilidad de generación o recombinación para ello recorro toda la matriz
     for i in range(eje_x):
@@ -79,9 +82,8 @@ for k in tqdm(range(0, num_pasos)):
                 # TODO: HACER UN REESCALADO DE LOS VALORES PARA EVITAR TENER QUE TRABAJAR CON NUMEROS TAN GRANDES
                 prob_generacion = Generation.generation(paso_temporal, Campo_Electrico, Temperatura)
                 if (i == 0 and j == 0):
-                    (ic(prob_generacion))
-                # genero un número aleatorio entre 0 y 1
-                random_number = np.random.rand()
+                    # genero un número aleatorio entre 0 y 1
+                    random_number = np.random.rand()
                 if random_number < prob_generacion:
                     actual_state[i, j] = 1  # Generación
             else:
@@ -94,9 +96,16 @@ for k in tqdm(range(0, num_pasos)):
                 if random_number < prob_recombinacion:
                     actual_state[i, j] = 0  # Recombinación
 
+    # Guardo los datos en el excel
+    df.loc[k] = [simulation_time, voltaje, Campo_Electrico, Corriente, Temperatura,
+                 prob_generacion, prob_recombinacion, Percolation.is_path(actual_state)]
+
     # Guardo el estado actual CADA paso_guardar PASOS MONTECARLO
     if k % paso_guardar == 0:
         configuraciones_matriz[int(k / paso_guardar) - 1] = actual_state
+
+# Guardo los datos en un excel
+df.to_excel('resultados.xlsx', index=False)
 
 # Supongamos que las imágenes están en el subdirectorio "Figuras" y tienen nombres de archivo que siguen el patrón "image*.png"
 filenames = glob.glob('Figuras/grafica*.png')
@@ -117,7 +126,7 @@ for matrix in tqdm(configuraciones_matriz):
     k += 1
 
 # Guardar las imágenes como un GIF
-images[0].save('animated_matrix.gif', save_all=True, append_images=images[1:], optimize=False, duration=30, loop=0)
+images[0].save('animated_matrix.gif', save_all=True, append_images=images[1:], optimize=False, duration=100, loop=0)
 
 # Imprimir un mensaje de éxito
 print("Las matrices se han guardado correctamente como un GIF en 'animated_matrix.gif'")
